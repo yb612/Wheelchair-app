90f53b2f19d3f09797abac0075fe85072443d5cd
yb612 <yb612@imperial.ac.uk>
2014-08-06 19:43:02 +0100

    getting closer to listing items

--- src/com/urop/wheelchair/DataActivity.java
+++ src/com/urop/wheelchair/DataActivity.java
@@ -1,16 +1,23 @@
 package com.urop.wheelchair;
 // the user enters their registered username and password to login to their account
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.dropbox.sync.android.DbxAccountManager;
+import com.dropbox.sync.android.DbxFile;
+import com.dropbox.sync.android.DbxFileInfo;
 import com.dropbox.sync.android.DbxFileSystem;
+import com.dropbox.sync.android.DbxPath;
 
 public class DataActivity extends ActionBarActivity {
 
@@ -31,6 +38,9 @@ public class DataActivity extends ActionBarActivity {
 
 	ListView l;
 	String[] days = {"sunday","monday"};
+	
+	
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -38,45 +48,52 @@ public class DataActivity extends ActionBarActivity {
 		setContentView(R.layout.activity_data);
 		mDbxAcctMgr = DbxAccountManager.getInstance(getApplicationContext(), APP_KEY, APP_SECRET);
 		
-		
-		 
-		    
-		 String[] fnames = null;
-
+		String[] some = new String[2];
+		List<DbxFileInfo> entries =  new ArrayList<DbxFileInfo>();
+		String[] test =  new String[entries.size()];
+		entries.toArray(test);
+		String[] fnames = new String[entries.size()];
 		 DbxPath path;
-		 path = new DbxPath("/");
+		 path = new DbxPath("/"); 
 		 
-         
-        if(isAuthenticated()==false || fs==null){
- 		Toast.makeText(this, "switched", Toast.LENGTH_SHORT).show();
- 		ArrayList<String> result = new ArrayList<String>();
- 		DbxFileSystem dbxFs;
-        }
-        Toast.makeText(this, "notswitched", Toast.LENGTH_SHORT).show();
-		 try {
-			 
-			 DbxFile dbFile;
-			 
-			 dbxFs = DbxFileSystem.forAccount(mDbxAcctMgr.getLinkedAccount());
-		     ArrayList<DbxFileInfo> entries=  (ArrayList<DbxFileInfo>) dbxFs.listFolder(path);
-		     
-		     int i=0;
-		     for (Entry ent: entries.contents) 
-	            {
-	                files.add(ent);// Add it to the list of thumbs we can choose from                       
-	                //dir = new ArrayList<String>();
-	                dir.add(new String(files.get(i++).path));
-	            }
-	            i=0;
-		     //fnames=entries.toArray(new String[entries.size()]);
-		} catch (Unauthorized e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (DbxException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		 
+	     ArrayList<String> result=  new ArrayList<String>();
+	     DbxFileSystem fs = getDbxFS();
+     	
 
+        if(isAuthenticated()==false || fs==null){
+        	Toast.makeText(this, "not switched", Toast.LENGTH_SHORT).show();
+
+        }
+        try {
+        	DbxFile dbFile;
+        	DbxPath dbPath = path;
+        	if (mDbxFs.exists(dbPath)){
+        		dbFile = mDbxFs.open(dbPath);
+        	}else{
+        		dbFile = mDbxFs.create(dbPath);
+        	}
+        	
+        	String [] lines = dbFile.readString().split("\r|\n|\r\n");
+            dbFile.close();
+            for (String line: lines) {
+                if (!line.trim().equals("")) {
+                    result.add(line);
+                }
+                
+            }
+            
+            int index = 0;
+            for (DbxFileInfo value: entries) {
+            	fnames[index] = (String) value.toString();
+            	index++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }	 
+			 
+		     
+		     
 		 
 
 		/*
@@ -99,10 +116,10 @@ public class DataActivity extends ActionBarActivity {
        
 		
 		l = (ListView)findViewById(R.id.list);
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,days);
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,test);
 		l.setAdapter(adapter);
 		
-		
+		Toast.makeText(this, "done loading", Toast.LENGTH_SHORT).show();
 		
 		//testing the list
 		
@@ -117,25 +134,28 @@ public class DataActivity extends ActionBarActivity {
 		
 	}
 	
-	 private DbxFileSystem getDbxFS () {
-	        if (mDbxFs!=null) {
-	            return mDbxFs;
-	        }
-	        if (isAuthenticated()) {
-	            try {
-	                this.mDbxFs = DbxFileSystem.forAccount(mDbxAcctMgr.getLinkedAccount());
-	                mDbxFs.awaitFirstSync();
-	                return mDbxFs;
-	            } catch (IOException e) {
-	                e.printStackTrace();
-	               
-	            }
-	        }
-	        return null;
-	    }
-	    static public String getDefaultPath() {
-	        return "/todo/todo.txt";
-	    }
+	
+	private DbxFileSystem getDbxFS () {
+        if (mDbxFs!=null) {
+            return mDbxFs;
+        }
+        if (isAuthenticated()) {
+            try {
+                this.mDbxFs = DbxFileSystem.forAccount(mDbxAcctMgr.getLinkedAccount());
+                mDbxFs.awaitFirstSync();
+                return mDbxFs;
+            } catch (IOException e) {
+                e.printStackTrace();
+               
+            }
+        }
+        return null;
+    }
+    static public String getDefaultPath() {
+        return "/todo/todo.txt";
+    }
+	 
+	    
 	    public boolean isAuthenticated() {
 	        if(mDbxAcctMgr!=null) {
 	            return mDbxAcctMgr.hasLinkedAccount();
@@ -145,22 +165,8 @@ public class DataActivity extends ActionBarActivity {
 	    }
 	    
 	    
-	    private DbxFileSystem getDbxFS () {
-	        if (mDbxFs!=null) {
-	            return mDbxFs;
-	        }
-	        if (isAuthenticated()) {
-	            try {
-	                this.mDbxFs = DbxFileSystem.forAccount(mDbxAcctMgr.getLinkedAccount());
-	                mDbxFs.awaitFirstSync();
-	                return mDbxFs;
-	            } catch (IOException e) {
-	                e.printStackTrace();
-	                throw new TodoException("Dropbox", e);
-	            }
-	        }
-	        return null;
-	    }
+	   
+	    
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
